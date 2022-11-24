0fbd2948753ccf15bfc934ad2b8e0d820096dce1
Adeel-kanju <adeel.kanju114@gmail.com>
2022-11-24 06:58:13 +0500

    add new class components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React,{Component} from 'react';
 import Navbar from './Components/Navbar';
+import News from './Components/News';
 export default class App extends Component {
   render(){
     return(
       <>
         <div>Helo class component </div>
         <Navbar></Navbar>
+        <News/>
       </>
     )
     
---
+++ src/Components/NewsItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+export default class NewsItem extends Component{
+    render(){
+        return(
+            <div>
+                this is news item components 
+            </div>
+        )
+    }
+}
