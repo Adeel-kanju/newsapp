c2fa5cf99b18da821fcba25bf749757700914527
Adeel-kanju <adeel.kanju114@gmail.com>
2022-11-27 21:48:41 +0500

    class base component fetch data

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
-export default class News extends Component{
-render(){
-    return(
-        <div>
-            this news component
-            <NewsItem/>
-        </div>
-    )
-}
+export default class News extends Component {
+   
+    constructor() {
+        super();
+        this.state = { articles: [] }
+    }
+    async componentDidMount(){
+        let apiUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=658e058b30114de99067859c7b29688f";
+        let data = await fetch(apiUrl);
+        let parsedata = await data.json();
+        this.setState({articles: parsedata.articles })
+    }
+    render() {
+        
+
+        return (
+            <div className='container my-3'>
+                <h1>Top News Headlines</h1>
+                <div className='row my-4'>
+                    {this.state.articles.map((element) => {
+                        return <div className='col-md-4' key={element.url}>
+                            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                        </div>
+                    })}
+
+
+                </div>
+            </div>
+
+        )
+    }
 } 
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 export default class NewsItem extends Component{
     render(){
+        let {title, description, imageUrl, newsUrl}= this.props
         return(
             <div>
-                this is news item components 
+               <div className="card" style={{width: "18rem"}}>
+                <img src={imageUrl? imageUrl: "https://ichef.bbci.co.uk/news/1024/branded_news/176CD/production/_127794959_tsinhuaprotest.jpg"} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href={newsUrl} className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
             </div>
         )
     }
